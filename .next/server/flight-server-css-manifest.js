e106ba4f375290eee178e2d2592a2e70386a6394
Asad-gsoft-consulting <137259850+Asad-gsoft-consulting@users.noreply.github.com>
2023-09-25 13:36:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"C:\\Users\\User\\Desktop\\Metaverse_clone\\app\\page":["C:\\Users\\User\\Desktop\\Metaverse_clone\\styles\\globals.css"]},"C:\\Users\\User\\Desktop\\Metaverse_clone\\app\\layout.js":["C:\\Users\\User\\Desktop\\Metaverse_clone\\styles\\globals.css"]}
